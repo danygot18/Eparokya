788a836b8bd92ed31aad5ca866613b1ca2cfc824
danygot18 <danygot74@gmail.com>
2025-02-09 10:41:22 +0800

    Picture

--- EparokyaWeb/src/socket/index.js
+++ EparokyaWeb/src/socket/index.js
@@ -4,7 +4,7 @@ const config = {
     autoConnect: false,
 };
 
-export const socket = io(`http://localhost:4001`, {
+export const socket = io(`https://eparokya.onrender.com`, {
     autoConnect: false,
     reconnection: true, // enables auto reconnection
     reconnectionAttempts: 10, // maximum attempts
