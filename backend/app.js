364b06356fe5e7691e0bbaff3c8537d7be6f069e
Dan <danygot74@gmail.com>
2025-06-15 05:21:29 +0800

    TestDeploy10

--- backend/app.js
+++ backend/app.js
@@ -15,7 +15,7 @@ const allowedOrigins = [
 
 // Middleware
 app.use(cors({
-  origin: ["https://eparokya.vercel.app", "http://localhost:3000"],
+  origin: ["https://eparokya.vercel.app", "http://localhost:3000", null],
   credentials: true,
   // methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   // allowedHeaders: ["Content-Type", "Authorization"],
