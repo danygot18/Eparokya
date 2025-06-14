eff49d8a61825ae2f4ee8290354d43a4e1891e1d
Dan <danygot74@gmail.com>
2025-06-15 06:54:29 +0800

    TestDeploy13

--- backend/app.js
+++ backend/app.js
@@ -17,8 +17,8 @@ const allowedOrigins = [
 app.use(cors({
   origin: allowedOrigins,
   credentials: true,
-  // methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  // allowedHeaders: ["Content-Type", "Authorization"],
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
 }));
 
 app.use(express.json());
