478125d449b19de3d80643e3548fd0e45e81f6e7
Dan <danygot74@gmail.com>
2025-06-15 04:13:18 +0800

    Test Deployweb

--- EparokyaWeb/src/socket/index.js
+++ EparokyaWeb/src/socket/index.js
@@ -1,45 +1,45 @@
-// import { io } from 'socket.io-client'
-
-// const config = {
-//     autoConnect: false,
-// };
-
-// export const socket = io(`https://eparokya.onrender.com`, {
-//     autoConnect: false,
-//     reconnection: true, // enables auto reconnection
-//     reconnectionAttempts: 10, // maximum attempts
-//     reconnectionDelay: 500, // initial delay in ms between attempts
-//     reconnectionDelayMax: 2000, // max delay in ms between reconnection attempts
-//     timeout: 5000, // connection timeout before failing
-// });
-
-// export const connectSocket = () => {
-//     if (!socket.connected) {
-//         socket.connect();
-//         console.log("Attempting to connect WebSocket...");
-//     } else {
-//         console.log("WebSocket already connected:", socket.id);
-//     }
-// };
-
-//Developing
-import { io } from 'socket.io-client';
+import { io } from 'socket.io-client'
 
 const config = {
-  autoConnect: false,
+    autoConnect: false,
 };
-const URL = process.env.NODE_ENV === 'production' 
-  ? 'https://your-production-url.com' 
-  : 'http://localhost:4001'; 
 
-// Use window.location.hostname to dynamically get the current IP/hostname
-export const socket = io(URL, {
-  withCredentials: true,
-  autoConnect: false,
-reconnection: true,
-  reconnectionAttempts: 10,
-  reconnectionDelay: 500,
-  reconnectionDelayMax: 2000,
-  timeout: 5000,
-
+export const socket = io(`https://eparokya.onrender.com`, {
+    autoConnect: false,
+    reconnection: true, // enables auto reconnection
+    reconnectionAttempts: 10, // maximum attempts
+    reconnectionDelay: 500, // initial delay in ms between attempts
+    reconnectionDelayMax: 2000, // max delay in ms between reconnection attempts
+    timeout: 5000, // connection timeout before failing
 });
+
+export const connectSocket = () => {
+    if (!socket.connected) {
+        socket.connect();
+        console.log("Attempting to connect WebSocket...");
+    } else {
+        console.log("WebSocket already connected:", socket.id);
+    }
+};
+
+//Developing
+// import { io } from 'socket.io-client';
+
+// const config = {
+//   autoConnect: false,
+// };
+// const URL = process.env.NODE_ENV === 'production' 
+//   ? 'https://your-production-url.com' 
+//   : 'http://localhost:4001'; 
+
+// // Use window.location.hostname to dynamically get the current IP/hostname
+// export const socket = io(URL, {
+//   withCredentials: true,
+//   autoConnect: false,
+// reconnection: true,
+//   reconnectionAttempts: 10,
+//   reconnectionDelay: 500,
+//   reconnectionDelayMax: 2000,
+//   timeout: 5000,
+
+// });
